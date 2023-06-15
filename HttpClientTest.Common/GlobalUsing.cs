0f262b37fa9e34fc0815bf9a318942c13b947544
Tanju BOZOK <tanjubozok@hotmail.com>
2023-06-16 00:25:05 +0300

    glabal using eklendi.

--- HttpClientTest.Common/GlobalUsing.cs
+++ HttpClientTest.Common/GlobalUsing.cs
@@ -1,3 +1,3 @@
-﻿global using HttpClientTest.Common.ComplexTypes;
-global using HttpClientTest.Common.Abstract;
+﻿global using HttpClientTest.Common.Abstract;
+global using HttpClientTest.Common.ComplexTypes;
 global using HttpClientTest.Entities.Abstract;
